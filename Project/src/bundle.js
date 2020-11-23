ca548511d2caef1e60af017ebe39b012568eeb27
Ernesto Aranda <ernestoadv@icloud.com>
2020-11-23 10:50:16 +0100

    Created some a working simple UI to control the video player. Can be tested by opening "/src/index.html".

--- Project/src/bundle.js
+++ Project/src/bundle.js
@@ -20573,9 +20573,17 @@ exports.renderHTML = require('./html').render;
 
 }).call(this)}).call(this,require("buffer").Buffer)
 },{"buffer":223,"stream":229,"string_decoder":244}],220:[function(require,module,exports){
+//Require the needed funcctions from the library created previously
 const { setContainer, open, play, pause, stop, seek, destroy } = require('@ernestoadv/tsplayer/lib')
 setContainer('videoPlayer')
 open('https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_640x360_800k.mpd')
+
+//Set button functionality
+document.getElementById('play').onclick = function(){play()}
+document.getElementById('pause').onclick = function(){pause()}
+document.getElementById('seek').onclick = function(){seek(parseInt(document.getElementById("seek-at").value))}
+document.getElementById('stop').onclick = function(){stop()}
+document.getElementById('destroy').onclick = function(){destroy()}
 
 },{"@ernestoadv/tsplayer/lib":1}],221:[function(require,module,exports){
 'use strict'
--- Project/src/main.js
+++ Project/src/main.js
@@ -1,3 +1,11 @@
+//Require the needed funcctions from the library created previously
 const { setContainer, open, play, pause, stop, seek, destroy } = require('@ernestoadv/tsplayer/lib')
 setContainer('videoPlayer')
 open('https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_640x360_800k.mpd')
+
+//Set button functionality
+document.getElementById('play').onclick = function(){play()}
+document.getElementById('pause').onclick = function(){pause()}
+document.getElementById('seek').onclick = function(){seek(parseInt(document.getElementById("seek-at").value))}
+document.getElementById('stop').onclick = function(){stop()}
+document.getElementById('destroy').onclick = function(){destroy()}
